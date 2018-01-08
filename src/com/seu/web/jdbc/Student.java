3baa6f1fecce27d83860a7a8ac162e5f345ca3de
wanghuawei <whw19950510@gmail.com>
2018-01-07 21:53:36 -0600

    add student model

---
+++ src/com/seu/web/jdbc/Student.java
@@ -0,0 +1,54 @@
+package com.seu.web.jdbc;
+
+public class Student {
+	private String firstname;
+	private String lastname;
+	private long id;
+	private String email;
+	
+	public Student(String firstname, String lastname, long id, String email) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.id = id;
+		this.email = email;
+	}
+	
+	public Student(String firstname, String lastname, String email) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	//used for debugging information
+	@Override
+	public String toString() {
+		return "Student [firstname=" + firstname + ", lastname=" + lastname + ", id=" + id + ", email=" + email + "]";
+	}
+	
+	
+}
---
+++ src/com/seu/web/jdbc/StudentDbUtil.java
@@ -0,0 +1,50 @@
+package com.seu.web.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+public class StudentDbUtil {
+	private DataSource datasource;
+	public StudentDbUtil(DataSource datasource) {
+		this.datasource = datasource;
+	}
+	public List<Student> getStudents() throws Exception{
+		List<Student> students = new ArrayList<>();
+		Connection myconn = null;
+		Statement myst = null;
+		ResultSet myrs = null;
+		
+		try {
+			myconn = datasource.getConnection();
+			String sqlqu = "select * from student order by last_name";
+			myst = myconn.createStatement();
+			myrs = myst.executeQuery(sqlqu);
+			while(myrs.next()) {
+				long Id = myrs.getInt("id");
+				String Firstname = myrs.getString("first_name");
+				String Lastname = myrs.getString("last_name");
+				String Email = myrs.getString("email");
+				Student curstu = new Student(Firstname, Lastname, Id, Email);
+				students.add(curstu);
+			}
+			return students;
+		} finally {
+			close(myconn,myst,myrs);
+		}		
+	}
+	private void close(Connection myconn, Statement myst, ResultSet myrs) {
+		// TODO Auto-generated method stub
+		try {
+			if(myconn != null) myconn.close();
+			if(myst != null) myst.close();
+			if(myrs != null) myrs.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
